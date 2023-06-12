d7bc42ed5e236f4aa57a744614f04d323da3de8e
victory balogun <victorybalogun@gmail.com>
2023-06-12 12:46:44 +0100

    First Commit with all files

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilters from "./CustomFilters";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilters,
+    SearchManufacturer,
+    CarCard,
+    CarDetails,
+    ShowMore
+};
